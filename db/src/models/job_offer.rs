3dae5882e386280b458b76e671731533911c8725
Matthieu Coraleau <matthieu.coraleau@epitech.eu>
2026-07-17 17:11:04 +0200

    feat(models/): add the job_offer struct who match job_offer table in db

---
+++ db/src/models/job_offer.rs
@@ -0,0 +1,26 @@
+use chrono::NaiveDateTime;
+use serde::{Deserialize, Serialize};
+use sqlx::FromRow;
+
+#[derive(Debug, Serialize, Deserialize, Clone, FromRow, Default)]
+pub struct JobOffer {
+    pub id: i32,
+    pub title: String,
+    pub description: String,
+    pub company: String,
+    pub location: String,
+    pub url: String,
+    pub score: i32,
+    pub created_at: NaiveDateTime,
+
+    pub salary: Option<String>,
+    pub study_level: Option<String>,
+    pub contract_type: Option<String>,
+    pub duration: Option<String>,
+    pub remote: Option<String>,
+    pub experience_level: Option<String>,
+}
+
+impl crate::models::Table for JobOffer {
+    const TABLE: &'static str = "job_offers";
+}
