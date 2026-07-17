fbbdd72859eeffb632e639f05b5ea4ccfbd92dd0
Matthieu Coraleau <matthieu.coraleau@epitech.eu>
2026-07-17 17:35:10 +0200

    fix(mods): update the migration mod file and the main lib.rs file with new modules

---
+++ db/src/lib.rs
@@ -0,0 +1,4 @@
+pub mod connection;
+pub mod models;
+pub mod orm;
+pub mod migration_cli;
---
+++ db/src/migration_cli/mod.rs
@@ -0,0 +1,4 @@
+pub mod run_migration_files;
+pub mod find_migrations;
+pub mod migration_cli;
+pub mod record_migration;
