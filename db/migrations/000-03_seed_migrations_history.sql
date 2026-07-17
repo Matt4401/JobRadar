9461dd6ac7215b84de39056a58b021a6f8bbee83
Matthieu Coraleau <matthieu.coraleau@epitech.eu>
2026-07-17 17:26:11 +0200

    feat(migrations/): add a third sql script for fill hirstory table and add a script example

---
+++ db/migrations/000-03_seed_migrations_history.sql
@@ -0,0 +1,4 @@
+INSERT INTO migrations_history (version, description, success) VALUES
+('000-01', 'Create job_offers table', TRUE),
+('000-02', 'Create migrations_history table', TRUE),
+('000-03', 'Seed migrations_history table', TRUE);
