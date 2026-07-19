469059eee173c09da3991d40a00994da4e3f3f02
Matthieu Coraleau <matthieu.coraleau@epitech.eu>
2026-07-19 13:49:30 +0200

    feat(workspace): finally pass the workspace intercations in shared crate

--- shared/src/workspace/workspace.rs
+++ shared/src/workspace/workspace.rs
@@ -6,3 +6,7 @@ pub fn workspace_root() -> PathBuf {
         .map(|p| p.to_path_buf())
         .unwrap_or_else(|| PathBuf::from("."))
 }
+
+pub fn workspace_root_join(path: &str) -> PathBuf {
+    Path::new(env!("CARGO_MANIFEST_DIR")).join(path)
+}
