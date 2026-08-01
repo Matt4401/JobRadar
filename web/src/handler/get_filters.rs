b8b587a910e50f859cd73b8f542ed83a22ed9393
Matthieu Coraleau <matthieu.coraleau@epitech.eu>
2026-08-01 12:51:18 +0200

    feat(get_filters.rs): add the last handler, the one for filter request since site

---
+++ web/src/handler/get_filters.rs
@@ -0,0 +1,7 @@
+use crate::render_filters;
+use axum::response::Html;
+
+async fn filters_page() -> Html<String> {
+    let filters = scraper::parser::filters::update_filters::load_filters();
+    Html(render_filters(&filters))
+}
