f7fea49e0f3043b2a6ca1d72e475c63a32d62477
Matthieu Coraleau <matthieu.coraleau@epitech.eu>
2026-08-01 15:19:51 +0200

    feat(main.rs): add the entry point of the web workspace

---
+++ web/src/main.rs
@@ -0,0 +1,35 @@
+mod handler;
+mod routing;
+mod state;
+mod views;
+
+use crate::routing::router::get_router;
+use db::migration_cli::migration_cli::establish_connection;
+use scraper::core::ScrapperConfig;
+use state::AppState;
+
+use serde::Deserialize;
+use shared::toml::toml_interactions::TomlHelper;
+
+#[derive(Deserialize)]
+pub struct AxumConfig {
+    pub port: String,
+}
+
+pub fn load_config() -> Result<AxumConfig, String> {
+    TomlHelper::new("axum_config.toml").read()
+}
+
+#[tokio::main]
+async fn main() {
+    let db = establish_connection().await;
+    let state = AppState { db };
+    let app = get_router(state).await;
+    let config = load_config().expect("Error loading axum_config.toml");
+    let addr = config.port;
+    let port_copy = addr.clone();
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+
+    println!("Job Scraper web server running on http://{}", port_copy);
+    axum::serve(listener, app).await.unwrap();
+}
