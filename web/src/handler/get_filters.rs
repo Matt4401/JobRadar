b63d3c7e3631a5d243672b91615c55297030712c
Matthieu Coraleau <matthieu.coraleau@epitech.eu>
2026-08-01 15:08:14 +0200

    refactor(get_filters.rs): chnage the return type for return an askama template

--- web/src/handler/get_filters.rs
+++ web/src/handler/get_filters.rs
@@ -1,7 +1,32 @@
 use crate::render_filters;
-use axum::response::Html;
+use crate::{
+    state::AppState,
+    views::{FiltersTemplate, IndexTemplate},
+};
+use askama::Template;
+use axum::{extract::State, http::StatusCode, response::Html, response::IntoResponse};
 
-pub async fn filters_page() -> Html<String> {
+pub async fn filters_page() -> impl IntoResponse {
     let filters = scraper::parser::filters::update_filters::load_filters();
-    Html(render_filters(&filters))
+    let min_salary_str = filters
+        .min_salary
+        .map(|v| v.to_string())
+        .unwrap_or_default();
+
+    let template = FiltersTemplate {
+        filters: &filters,
+        min_salary_str,
+    };
+
+    match template.render() {
+        Ok(html) => Html(html).into_response(),
+        Err(e) => {
+            eprintln!("Template error (filters): {e}");
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "Error rendering template",
+            )
+                .into_response()
+        }
+    }
 }
